e028ee9bf03902a435adc00dd0583ab6dcaf0ad5
talystbuild <talystbuild@gmail.com>
2015-03-12 10:17:47 -0700

    test file

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+type Status int
+
+const (
+	StatusOk Status = iota
+	StatusError
+)
+
+func (s Status) String() string {
+	return fmt.Sprintf("Status-%d", s)
+}
+
+func main() {
+	fmt.Println(StatusError.String())
+}
